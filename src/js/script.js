a303e05294d8ddd917d5ae507b41f632d9e4e620
Marcos Freitas <freitas.ma.1993@gmail.com>
2023-05-31 22:47:04 +0100

    Commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,18 @@
 // Check if DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.body;
-  const navbar = document.getElementById('navbar');
-  const rotate = document.getElementById('rotate-img');
-// Traking scroll position
+  const navbar = document.querySelector('#navbar');
+  const rotate = document.querySelector('#rotate-img');
+  const albums = document.querySelectorAll('.album');
+
+  // Traking scroll position
   function getVisibleScrollPercentage() {
     const totalHeight = body.scrollHeight - body.clientHeight;
-    console.log(totalHeight, 'total height');
     const scrollPosition = body.scrollTop;
-    console.log(scrollPosition, 'scroll position');
-    return (scrollPosition / totalHeight) * 100;
+    return Math.round((scrollPosition / totalHeight) * 100);
   }
-// Verifying if element is visible
+
+  // Verifying if element is visible
   function isElementVisible(element) {
     const rect = element.getBoundingClientRect();
     const windowHeight = window.innerHeight;
@@ -20,31 +21,49 @@ document.addEventListener('DOMContentLoaded', () => {
     const visibleHeight = windowHeight * (visiblePercentage / 100);
     return visibleHeight >= elementHeight;
   }
-//  
+
+  // Listen for scroll events on the body
   body.addEventListener('scroll', () => {
     const scrollPercentage = getVisibleScrollPercentage();
-    console.log(scrollPercentage, 'posição do scroll');
+    console.log(`Scroll percentage: ${scrollPercentage}%`);
+
+    // Add or remove classes on the navbar depending on scroll position
     if (scrollPercentage > 2) {
-      console.log('entrou');
       navbar.classList.add('scrolled');
       navbar.classList.remove('navbar-transparent');
-
-    } 
-    if (scrollPercentage < 2) {
+    } else {
       navbar.classList.remove('scrolled');
       navbar.classList.add('navbar-transparent');
     }
-    if (scrollPercentage > 45) {
-      rotate.classList.remove('reverse-rotate-and-fade');
-      rotate.classList.add('rotate-and-fade');
-    }
-    if (scrollPercentage < 45) {
-      rotate.classList.remove('rotate-and-fade');
-      rotate.classList.add('reverse-rotate-and-fade');
+
+    // Add or remove classes on the albums depending on scroll position
+    albums.forEach(album => {
+      if (isElementVisible(album)) {
+        album.classList.add('album-animation');
+        album.classList.remove('album-noAnimation');
+      } else {
+        album.classList.remove('album-animation');
+        album.classList.add('album-noAnimation');
+      }
+    });
+
+    // Add or remove classes on the rotate element depending on scroll position
+    if (isElementVisible(rotate)) {
+      console.log('Rotate is visible');
+      if (!rotate.classList.contains('rotate-and-fade')) {
+        rotate.classList.remove('reverse-rotate-and-fade');
+        rotate.classList.add('rotate-and-fade');
+      }
+    } else {
+      if (!rotate.classList.contains('reverse-rotate-and-fade')) {
+        rotate.classList.remove('rotate-and-fade');
+        rotate.classList.add('reverse-rotate-and-fade');
+      }
     }
   });
 });
 
+// Log the window width and device type on resize events
 function logWindowWidth() {
   const width = window.innerWidth;
   console.log(`Window width: ${width}px`);
